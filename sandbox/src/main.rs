b973db601941f4c77406a3b7edc499885e85df9e
Iurii Korolev <acollin@mi-o5.msk.21-school.ru>
2023-12-04 21:16:13 +0300

    Chapter 5: understanding refs

---
+++ sandbox/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    println!(r###"
+        This raw string started with 'r###"'.
+        Therefore it does not end until we reach a quote mark ('"')
+        followed immediately by three pound signs ('###'):
+    "###);
+}
